af6623160b8215e83b7fc8976a6ddcd4046cb231
VINICIUS MAFRA LOPES <vinicius.lopes@facens.br>
2022-08-24 23:40:02 -0300

    Documentação

--- LoginPage.js
+++ LoginPage.js
@@ -1,12 +1,15 @@
-const { chromium } = require ('playwright')
+//Criar extensão do navegador
+const { chromium } = require ('playwright');
 
-(async() => {
+( async() => {
+    //Como padrão do Playwright, ele ira rodar o script na ordem que foi desenvolvida  
     //Criando um navegador para test
-     const browser = await chromium.launch();
+     const browser = await chromium.launch({handless:false, slowMo: 100});
+    //Nescessario que aplique a função de diminuir velocidade de execução "slowMo:milissegundos"
     //Criando uma pagina do navegador
-    const page = await browser.newPage()
+    const page = await browser.newPage();
     //url de navegação para pagina
-    await page.goto('http://google.com')
+    await page.goto('https://github.com/ViniciusMafraL');
     //Fechar navegador apos finalizar test
     await browser.close();
-})
+})();
